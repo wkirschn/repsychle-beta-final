4524ce4b277eaea0ea3fb67dac61c0dfa93db1be
Wyatt <wkirschn@uoguelph.ca>
2022-04-23 15:53:06 -0400

    Completed:
    301, 401, 404, 500, 502, About

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,78 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+router.use(express.urlencoded({extended: false}))
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET 301 Error page. */
+router.get('/301', function(req, res, next) {
+  res.render('301');
+});
+
+/* GET 401 Error page. */
+router.get('/401', function(req, res, next) {
+  res.render('401');
+});
+
+/* GET 404 Error page. */
+router.get('/404', function(req, res, next) {
+  res.render('404');
+});
+
+/* GET 500 Error page. */
+router.get('/500', function(req, res, next) {
+  res.render('500');
+});
+
+
+/* GET 502 Error page. */
+router.get('/502', function(req, res, next) {
+  res.render('502');
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+
+/* GET Frequently Asked Questions page. */
+router.get('/faq', function(req, res, next) {
+  res.render('faq');
+});
+
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET Register page. */
+router.get('/register', (req, res, next) => {
+  /*    const messages = req.flash();
+      res.render('register', {messages})*/
+  res.render('register');
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
