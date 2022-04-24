8765929e2d70134726a29c47b81d97628f23483f
Wyatt <wkirschn@uoguelph.ca>
2022-04-24 15:47:03 -0400

    I want to change the way the View for each item is individually

--- app.js
+++ app.js
@@ -467,7 +467,7 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('500');
 });
 
 module.exports = app;
